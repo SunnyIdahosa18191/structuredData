8931265c56c4226b4df4019948ca0f05517f06f6
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-10 13:52:47 +0300

    Fix issues with deployment scripts

---
+++ MyLibrary/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Car
+    {
+        public int? Train_number { get; set; }
+        public string Type { get; set; }
+        public int Seat_quantity { get; set; }
+        public Car(int? Train_number, string Type, int Seat_quantity)
+        {
+            this.Train_number = Train_number;
+            this.Type = Type;
+            this.Seat_quantity = Seat_quantity;
+        }
+        public override string ToString()
+        {
+            return string.Format($"Номер потягу - {Train_number}; Тип - {Type}; Кількість місць - {Seat_quantity}");
+        }
+    }
+}
